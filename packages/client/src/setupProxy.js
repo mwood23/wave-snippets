27b84fdc29fac0e568e33fdd0c4ee728429986fe
Marcus Wood <marcuswood230@gmail.com>
2020-07-07 21:46:19 -0400

    fix linter in app

--- packages/client/src/setupProxy.js
+++ packages/client/src/setupProxy.js
@@ -1,12 +1,14 @@
-const proxy = require('http-proxy-middleware');
+/* eslint-disable import/no-commonjs */
+// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
+const proxy = require('http-proxy-middleware')
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/api',
     proxy({
       target: process.env.REACT_APP_CLOUD_FUNCTIONS_URL,
       changeOrigin: true,
-      pathRewrite: {'^/api' : ''}
-    })
-  );
-};
+      pathRewrite: { '^/api': '' },
+    }),
+  )
+}
