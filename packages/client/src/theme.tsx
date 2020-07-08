ae4ab6217dd429cceda52f9bae4f2071ee207281
Marcus Wood <marcuswood230@gmail.com>
2020-07-07 21:54:51 -0400

    pass no tests

--- packages/client/src/theme.tsx
+++ packages/client/src/theme.tsx
@@ -6,11 +6,6 @@ export const customTheme = {
   ...theme,
   colors: {
     ...theme.colors,
-    brand: {
-      900: '#1a365d',
-      800: '#153e75',
-      700: '#2a69ac',
-    },
   },
   icons: {
     ...theme.icons,
